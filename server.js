439b50c4589c373bf29a56c16d8bc93618202ee6
balijawa hussein <balijawahussein@gmail.com>
2024-09-13 02:26:05 +0300

    form control is ready

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+// 01 connection url
+mongoose.connect("mongodb://localhost:27017/TKTUsers");
+// 02 creating schema
+const userSchema = new mongoose.Schema({
+    fName:String,
+    lName:String,
+    email:String,
+    password:String,
+    rePassword:String
+}); 
+// 03 creating collection
+const Users = new mongoose.model("Users", userSchema);
+// 04 inserting new data
 
 const app = express();
 const port = 3000;
@@ -8,6 +23,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+function passwordsMatch(password, rePassword) {
+    return password === rePassword;
+}
 
 // routes
 // home
@@ -28,7 +47,16 @@ app.post("/login",(req,res)=>{
     res.render("logIn",{dontMatch:""});
 });
 app.post("/signUp",(req,res)=>{
-    console.log(req.body);
+    const {fName,lName, email, password, rePassword}= req.body;
+    const user = new Users({
+        fName:fName,
+        lName:lName,
+        email:email,
+        password:password,
+        rePassword:rePassword
+    });
+    // user.save();
+    console.log(user);
     res.render("signUp",{dontMatch:""});
 });
 app.listen(port,()=>{
