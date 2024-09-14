026d8188b5aa797ad2138bece63198dd0a1cdf56
balijawa hussein <balijawahussein@gmail.com>
2024-09-15 02:49:08 +0300

    form can now take in user details and authenticate on level two

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+const port = 3000;
 
 // 01 connection url
 mongoose.connect("mongodb://localhost:27017/TKTUsers");
@@ -13,21 +18,18 @@ const userSchema = new mongoose.Schema({
     password:String,
     rePassword:String
 }); 
+// 01 level encryption
+userSchema.plugin(encrypt,{secret:process.env.SECRETS, encryptedFields:["password", "rePassword"]});
 // 03 creating collection
 const Users = new mongoose.model("Users", userSchema);
 // 04 inserting new data
-
-const app = express();
-const port = 3000;
-
 app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 function passwordsMatch(password, rePassword) {
     return password === rePassword;
 }
-
 // routes
 // home
 app.get("/",(req, res)=>{
@@ -42,23 +44,41 @@ app.get("/signUp",(req, res)=>{
     res.render("signUp",{dontMatch:""});
 });
 // handle forms
-app.post("/login",(req,res)=>{
-    console.log(req.body);
-    res.render("logIn",{dontMatch:""});
-});
+
 app.post("/signUp",(req,res)=>{
     const {fName,lName, email, password, rePassword}= req.body;
-    const user = new Users({
+    const User = new Users({
         fName:fName,
         lName:lName,
         email:email,
         password:password,
         rePassword:rePassword
     });
-    // user.save();
-    console.log(user);
-    res.render("signUp",{dontMatch:""});
+    if(User.password === User.rePassword){
+        User.save();
+        res.send("user saved to db");
+    }else{
+        res.render("signUp",{dontMatch:"passwords dont match"});
+    }
+});
+app.post("/login",async (req,res)=>{
+    const {email, password}= req.body;
+    try{
+        const foundUser = await Users.findOne({email:email});
+      if(foundUser){
+        console.log(foundUser);
+        if(foundUser.password === password ){
+            console.log("user found");
+            res.status(200).send("user found");
+        }else{
+            console.log("pussy");
+            res.render("logIn",{dontMatch:"user password is wrong"});
+        }
+      }else{res.status(401).render("logIn",{dontMatch:"no user found with following email"});;
+
+      }
+    }catch(err){console.log(err)};
 });
 app.listen(port,()=>{
-    console.log("server running on port 3000");
+    console.log(" server running on http://localhost:3000/");
 });
